f0be9bb33f1948a8e6fd9b98b32543933b655f42
saransh <saranshbalyan1234@gmail.com>
2025-09-25 00:44:15 +0530

    logs

--- src/routes/api/autohooks.ts
+++ src/routes/api/autohooks.ts
@@ -1,6 +1,19 @@
 import { FastifyInstance } from 'fastify'
 
 export default async function (fastify: FastifyInstance) {
+
+  fastify.addHook('preHandler', function (req, reply, done) {
+  if (req.body) {
+    req.log.info({payload:req.body}, 'request body')
+  }
+  done()
+})
+
+fastify.addHook('onSend', function (req, reply, payload, done) {
+  req.log.info({response:payload}, 'response body');
+  done();
+});
+
   fastify.addHook('onRequest', async (request, reply) => {
     if (request.url.startsWith('/api/auth/login')) {
       return
