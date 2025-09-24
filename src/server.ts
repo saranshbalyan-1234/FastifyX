f7f37bfdddeac8d4ed0db85484536d4020ca2d82
saransh <saranshbalyan1234@gmail.com>
2025-09-25 00:00:00 +0530

    /

--- src/server.ts
+++ src/server.ts
@@ -14,30 +14,18 @@ import closeWithGrace from 'close-with-grace'
 // Import your application as a normal plugin.
 import serviceApp from './app.js'
 
-/**
- * Do not use NODE_ENV to determine what logger (or any env related feature) to use
- * @see {@link https://www.youtube.com/watch?v=HMM7GJC5E2o}
- */
-function getLoggerOptions () {
-  // Only if the program is running in an interactive terminal
-  if (process.stdout.isTTY) {
-    return {
-      level: 'info',
-      transport: {
-        target: 'pino-pretty',
-        options: {
-          translateTime: 'HH:MM:ss Z',
-          ignore: 'pid,hostname'
-        }
+const app = Fastify({
+  logger: {
+    level: 'info',
+    transport: {
+      target: 'pino-pretty', // Use pino-pretty for string logs
+      options: {
+        translateTime: "UTC:yyyy-mm-dd'T'HH:MM:ss.l'Z'", // Human-readable time
+        singleLine: true, // Print logs in a single line
+        ignore: 'pid,hostname' // Remove pid and hostname from logs
       }
     }
-  }
-
-  return { level: process.env.LOG_LEVEL ?? 'silent' }
-}
-
-const app = Fastify({
-  logger: getLoggerOptions(),
+  },
   ajv: {
     customOptions: {
       coerceTypes: 'array', // change type of data to match type keyword
