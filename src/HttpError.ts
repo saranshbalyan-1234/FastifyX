d9b642d5d33b93448018dad45c44cf713bf33fda
saransh <saranshbalyan1234@gmail.com>
2025-09-28 03:10:43 +0530

    http Error

---
+++ src/HttpError.ts
@@ -0,0 +1,33 @@
+const OriginalError = global.Error;
+
+// Internal name doesn’t matter
+class Error extends OriginalError {
+  statusCode: number;
+  type: string;
+
+  constructor(message: string, statusCode = 500) {
+    super(message);
+
+    Object.setPrototypeOf(this, new.target.prototype);
+
+    if (OriginalError.captureStackTrace) {
+      OriginalError.captureStackTrace(this, this.constructor);
+    }
+
+    this.statusCode = statusCode;
+
+    // This is the value you want for logs / API responses
+    this.type = "HttpError";
+  }
+}
+
+// Attach globally
+(global as any).HttpError = Error;
+
+// Typings for TypeScript
+declare global {
+  var HttpError: typeof Error;
+}
+
+// Export for direct import if needed
+export { Error as HttpError };
